f793c78a7a1aa243ea843e902e7e399bc12a205c
diegosmen111 <dbav_10@hotmail.com>
2023-08-18 02:24:56 -0700

    fix 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,50 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+'''A simple Flask web application.
+'''
+from flask import Flask, render_template
+
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states(id=None):
+    '''The states page.'''
+    states = None
+    state = None
+    all_states = list(storage.all(State).values())
+    case = 404
+    if id is not None:
+        res = list(filter(lambda x: x.id == id, all_states))
+        if len(res) > 0:
+            state = res[0]
+            state.cities.sort(key=lambda x: x.name)
+            case = 2
+    else:
+        states = all_states
+        for state in states:
+            state.cities.sort(key=lambda x: x.name)
+        states.sort(key=lambda x: x.name)
+        case = 1
+    ctxt = {
+        'states': states,
+        'state': state,
+        'case': case
+    }
+    return render_template('9-states.html', **ctxt)
 
 
 @app.teardown_appcontext
-def appcontext_teardown(self):
-    """use storage for fetching data from the storage engine
-    """
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def state_info():
-    """Display a HTML page inside the tag BODY"""
-    return render_template('7-states_list.html',
-                           states=storage.all(State))
-
-
-@app.route('/states/<string:id>', strict_slashes=False)
-def state_id(id=None):
-    """Display a HTML page inside the tag BODY"""
-    return render_template('9-states.html',
-                           states=storage.all(State)
-                           .get('State.{}'.format(id)))
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
