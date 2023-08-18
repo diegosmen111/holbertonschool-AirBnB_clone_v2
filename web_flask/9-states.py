678d9e9b1eb49b8de7568bb4b590fa7db6384930
diegosmen111 <dbav_10@hotmail.com>
2023-08-18 02:10:34 -0700

    fix cities by states and task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
+"""
+starts a Flask web application
+"""
 from models import storage
 from models.state import State
 from flask import Flask, render_template
